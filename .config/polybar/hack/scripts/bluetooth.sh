bd93c2f7569104a7e48621b4886fdf4241c5823f
raold <raoldham@gmail.com>
2025-12-28 15:02:50 -0500

    chamonix changes

--- .config/polybar/hack/scripts/bluetooth.sh
+++ .config/polybar/hack/scripts/bluetooth.sh
@@ -2,15 +2,15 @@
 
 # Check if bluetooth service is running
 if ! systemctl is-active --quiet bluetooth; then
-    # Service disabled - show off icon
-    echo "%{F#928374}󰂲%{F-}"
+    # Service disabled - show off icon (grayed)
+    echo "%{F#928374}%{F-}"
     exit 0
 fi
 
 # Check if bluetooth is powered on
 if ! bluetoothctl show 2>/dev/null | grep -q "Powered: yes"; then
-    # Powered off - show off icon
-    echo "%{F#928374}󰂲%{F-}"
+    # Powered off - show off icon (grayed)
+    echo "%{F#928374}%{F-}"
     exit 0
 fi
 
@@ -19,8 +19,8 @@ connected=$(bluetoothctl devices Connected 2>/dev/null | head -1 | cut -d' ' -f3
 
 if [ -n "$connected" ]; then
     # Connected - show device name
-    echo "󰂱 $connected"
+    echo " $connected"
 else
     # On but not connected
-    echo "󰂯"
+    echo ""
 fi
